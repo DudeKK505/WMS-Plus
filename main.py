692dcc4ff24bb1fa464ad71a41190e8772fe026d
DudeKK505 <81750946+DudeKK505@users.noreply.github.com>
2026-03-29 19:16:22 +0200

    Reformat main.py by removing unnecessary print statements

--- main.py
+++ main.py
@@ -2,10 +2,8 @@ import scraper_id
 import scraper_ql
 from concurrent.futures import ThreadPoolExecutor
 
-print("Pobieranie zamówień, prosze czekać")
 scraper_id.scrape_id()
 
-print("Procesor sie poci by pobrac QL, czekaj")
 orders = scraper_ql.get_order_ids()
 
 with ThreadPoolExecutor(max_workers=20) as executor:
